8ada533d42702378f811cfef089a79f203ff166e
Sumit Raj <toppersumitr@iitkgp.ac.in>
2021-08-03 11:22:11 +0530

    added like delete and update functionality

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,21 @@
-import React,{ useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react';
 import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
 import { useDispatch } from 'react-redux';
-import { getPosts } from './actions/posts';
+
 import Posts from './components/Posts/Posts';
 import Form from './components/Form/Form';
+import { getPosts } from './actions/posts';
+import useStyles from './styles';
 import memories from './images/memories.png';
-import useStyle from './styles';
 
 const App = () => {
+  const [currentId, setCurrentId] = useState(0);
   const dispatch = useDispatch();
-  const classes=useStyle();
+  const classes = useStyles();
 
   useEffect(() => {
     dispatch(getPosts());
-  }, [dispatch]);
+  }, [currentId, dispatch]);
 
   return (
     <Container maxWidth="lg">
@@ -23,20 +25,18 @@ const App = () => {
       </AppBar>
       <Grow in>
         <Container>
-          <Grid container justifyContent="space-between" alignItems="stretch" spacing={3}>
+          <Grid container justify="space-between" alignItems="stretch" spacing={3}>
             <Grid item xs={12} sm={7}>
-              {/* <Posts setCurrentId={setCurrentId} /> */}
-              <Posts></Posts>
+              <Posts setCurrentId={setCurrentId} />
             </Grid>
             <Grid item xs={12} sm={4}>
-              {/* <Form currentId={currentId} setCurrentId={setCurrentId} /> */}
-              <Form></Form>
+              <Form currentId={currentId} setCurrentId={setCurrentId} />
             </Grid>
           </Grid>
         </Container>
       </Grow>
     </Container>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -2,35 +2,41 @@ import React, { useState, useEffect } from 'react';
 import { TextField, Button, Typography, Paper } from '@material-ui/core';
 import { useDispatch, useSelector } from 'react-redux';
 import FileBase from 'react-file-base64';
-import { createPost } from '../../actions/posts';
+
 import useStyles from './styles';
+import { createPost, updatePost } from '../../actions/posts';
 
-const Form=()=>{
-    const [postData, setPostData] = useState({ creator: '', title: '', message: '', tags: '', selectedFile: '' });
-    const classes = useStyles();
-    const dispatch = useDispatch();
+const Form = ({ currentId, setCurrentId }) => {
+  const [postData, setPostData] = useState({ creator: '', title: '', message: '', tags: '', selectedFile: '' });
+  const post = useSelector((state) => (currentId ? state.posts.find((message) => message._id === currentId) : null));
+  const dispatch = useDispatch();
+  const classes = useStyles();
 
-    const clear = () => {
-        // setCurrentId(0);
-        setPostData({ creator: '', title: '', message: '', tags: '', selectedFile: '' });
-      };
+  useEffect(() => {
+    if (post) setPostData(post);
+  }, [post]);
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-    
-        // if (currentId === 0) {
-          dispatch(createPost(postData));
-        //   clear();
-        // } else {
-        //   dispatch(updatePost(currentId, postData));
-        //   clear();
-        // }
-      };
+  const clear = () => {
+    setCurrentId(0);
+    setPostData({ creator: '', title: '', message: '', tags: '', selectedFile: '' });
+  };
 
-    return(
-        <Paper className={classes.paper}>
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (currentId === 0) {
+      dispatch(createPost(postData));
+      clear();
+    } else {
+      dispatch(updatePost(currentId, postData));
+      clear();
+    }
+  };
+
+  return (
+    <Paper className={classes.paper}>
       <form autoComplete="off" noValidate className={`${classes.root} ${classes.form}`} onSubmit={handleSubmit}>
-        <Typography variant="h6"> Creating a Memory</Typography>
+        <Typography variant="h6">{currentId ? `Editing "${post.title}"` : 'Creating a Memory'}</Typography>
         <TextField name="creator" variant="outlined" label="Creator" fullWidth value={postData.creator} onChange={(e) => setPostData({ ...postData, creator: e.target.value })} />
         <TextField name="title" variant="outlined" label="Title" fullWidth value={postData.title} onChange={(e) => setPostData({ ...postData, title: e.target.value })} />
         <TextField name="message" variant="outlined" label="Message" fullWidth multiline rows={4} value={postData.message} onChange={(e) => setPostData({ ...postData, message: e.target.value })} />
@@ -40,8 +46,7 @@ const Form=()=>{
         <Button variant="contained" color="secondary" size="small" onClick={clear} fullWidth>Clear</Button>
       </form>
     </Paper>
-    )
-}
+  );
+};
 
-export default Form
-
+export default Form;
